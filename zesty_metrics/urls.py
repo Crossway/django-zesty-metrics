ee4692efded4d1eac94be066b876d5ee7c6472d0
eykd <david.eyk@gmail.com>
2015-07-02 09:42:07 -0700

    Reindent urls.

--- zesty_metrics/urls.py
+++ zesty_metrics/urls.py
@@ -4,20 +4,24 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 
-urlpatterns = patterns('',
-                       url(r'^incr/(?P<stat>[^/]+)/?',
-                           csrf_exempt(views.IncrView.as_view()),
-                           name="metrics_incr"),
-                       url(r'^decr/(?P<stat>[^/]+)/?',
-                           csrf_exempt(views.DecrView.as_view()),
-                           name="metrics_decr"),
-                       url(r'^timing/(?P<stat>[^/]+)/?',
-                           csrf_exempt(views.TimingView.as_view()),
-                           name="metrics_timing"),
-                       url(r'^gauge/(?P<stat>[^/]+)/?',
-                           csrf_exempt(views.GaugeView.as_view()),
-                           name="metrics_gauge"),
-                       url(r'^report-request-rendered/(?P<request_id>[^/]+)/?',
-                           csrf_exempt(views.RequestTimingReportView.as_view()),
-                           name="metrics_report_request_rendered")
-                       )
+urlpatterns = patterns(
+    '',
+    url(r'^activity/(?P<what>[^/]+)/?',
+        csrf_exempt(views.ActivityView.as_view()),
+        name="metrics_activity"),
+    url(r'^incr/(?P<stat>[^/]+)/?',
+        csrf_exempt(views.IncrView.as_view()),
+        name="metrics_incr"),
+    url(r'^decr/(?P<stat>[^/]+)/?',
+        csrf_exempt(views.DecrView.as_view()),
+        name="metrics_decr"),
+    url(r'^timing/(?P<stat>[^/]+)/?',
+        csrf_exempt(views.TimingView.as_view()),
+        name="metrics_timing"),
+    url(r'^gauge/(?P<stat>[^/]+)/?',
+        csrf_exempt(views.GaugeView.as_view()),
+        name="metrics_gauge"),
+    url(r'^report-request-rendered/(?P<request_id>[^/]+)/?',
+        csrf_exempt(views.RequestTimingReportView.as_view()),
+        name="metrics_report_request_rendered"),
+)
