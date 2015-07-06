5b49934615cd5d6e0df4ea6450fdf6fd8ca8af70
eykd <david.eyk@gmail.com>
2015-07-06 13:36:34 -0700

    Add migrations.

---
+++ tests/south_settings.py
@@ -0,0 +1,25 @@
+DEBUG = True
+
+SECRET_KEY = 'fake-key'
+INSTALLED_APPS = [
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.admin',
+    'tests',
+    'zesty_metrics',
+    'south',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+
+ROOT_URLCONF = 'tests.urls'
+
+ZESTY_TRACKING_CLASSES = (
+    'tests.trackers.TestTracker',
+    )
