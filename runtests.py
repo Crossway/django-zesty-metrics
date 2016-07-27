4d2ceeff802c6925244f62eab0c2919a66a12e93
Ryan Rubin <ryanmrubin@gmail.com>
2016-07-27 15:15:47 -0500

    remove pre-Django 1.8 compat

--- runtests.py
+++ runtests.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os, sys
-os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
 from django.conf import settings
+from django.test.runner import DiscoverRunner
 
-try:
-    # Django <= 1.8
-    from django.test.simple import DjangoTestSuiteRunner
-    test_runner = DjangoTestSuiteRunner(verbosity=1)
-except ImportError:
-    # Django >= 1.8
-    from django.test.runner import DiscoverRunner
-    test_runner = DiscoverRunner(verbosity=1)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
+
+test_runner = DiscoverRunner(verbosity=1)
 
 failures = test_runner.run_tests(['zesty_metrics'])
 if failures:
