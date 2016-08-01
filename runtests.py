2a3bcb69e4e183ded02b68ef89e5d954235c3b11
Ryan Rubin <ryanmrubin@gmail.com>
2016-08-01 12:32:07 -0500

    setup django apps before running tests

--- runtests.py
+++ runtests.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os, sys
+import django
 from django.conf import settings
 from django.test.runner import DiscoverRunner
 
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
+django.setup()
 
 test_runner = DiscoverRunner(verbosity=1)
 
